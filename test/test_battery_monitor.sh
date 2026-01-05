90b6f24d48d1f13043588f1f3adbdc9eb6e076ae
Keigo Yamaguchi <k5poohsan1108@gmail.com>
2026-01-06 04:51:49 +0900

    Fix test to source ROS 2 environment and check exit status

--- test/test_battery_monitor.sh
+++ test/test_battery_monitor.sh
@@ -4,16 +4,16 @@
 
 set -e
 
-cd /root/ros2_ws
-source install/setup.bash
+source /root/ros2_ws/install/setup.bash
 
 ros2 launch ros2_topic_guard battery_monitor_launch.py &
 PID=$!
 
 sleep 5
-
-ros2 topic echo /battery_state -n 1 | grep -E "data: [0-2]"
-
 kill $PID || true
 
-echo "Launch and topic test completed"
+ros2 topic echo /battery_state -n 1 > /tmp/battery_state.log
+
+grep -E "data: [0-2]" /tmp/battery_state.log
+
+echo "Battery monitor test passed"
